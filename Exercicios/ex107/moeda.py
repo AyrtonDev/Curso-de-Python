ffc8d8748f5fe46b01178b6bbcb31fe2d76ef5e5
Ayrton G. Guerra <ayrtong.guerra@gmail.com>
2020-12-08 16:06:47 -0400

    1/6 exercicio de modularizaçao

---
+++ Exercicios/ex107/moeda.py
@@ -0,0 +1,17 @@
+def metade(n):
+    return n / 2
+
+
+def dobro(n):
+    return n * 2
+
+
+def aumentar(n, por):
+    p = n + (n * por / 100)
+    return p
+
+
+def diminuir(n, por):
+    p = n - (n * por / 100)
+    return p
+
