c35b3db5444f23d8588ba44775632a28ea68df7b
Ayrton G. Guerra <ayrtong.guerra@gmail.com>
2020-12-08 19:25:25 -0400

    Revert "3/6 Corregindo erro ne retorno de valor das defs"
    This reverts commit d39a648c6c096932612e324d0afded832c0cdf7d.

--- Exercicios/ex109/moeda.py
+++ Exercicios/ex109/moeda.py
@@ -1,11 +1,11 @@
 def metade(n=0, format=False):
     res = n / 2
-    return res if format is False else moeda(res)
+    return res if format is False else moeda(n)
 
 
 def dobro(n=0, format=False):
     res = n * 2
-    return res if format is False else moeda(res)
+    return res if format is False else moeda(n)
 
 
 def aumentar(n=0, por=0, format=False):
@@ -16,12 +16,12 @@ def aumentar(n=0, por=0, format=False):
     :param format: Para ativar a formatação de moeda
     '''
     p = n + (n * por / 100)
-    return p if format is False else moeda(p)
+    return p if format is False else moeda(n)
 
 
 def diminuir(n=0, por=0, format=False):
     p = n - (n * por / 100)
-    return p if format is False else moeda(p)
+    return p if format is False else moeda(n)
 
 
 def moeda(n=0, moeda = 'R$'):
